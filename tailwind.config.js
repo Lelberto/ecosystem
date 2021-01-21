4ec02f50fbddf42b8879d18323b7eb95ee29a00f
Lelberto <jsurieux@gmail.com>
2021-01-21 21:07:21 +0100

    Setup Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    purge: {
+        enabled: false,
+        content: [
+            './src/**/*.tsx'
+        ]
+    },
+    darkMode: false,
+    theme: {
+        extend: {}
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
