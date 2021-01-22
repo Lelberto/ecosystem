4486bc45fb308629a55d6c803b004656d6d48fb0
Lelberto <jsurieux@gmail.com>
2021-01-22 16:23:38 +0100

    Add size and color attributes to the entity class

--- src/logic/entities/entity.ts
+++ src/logic/entities/entity.ts
@@ -1,4 +1,5 @@
 import { Location } from '../utils/location';
+import { Vector2 } from '../utils/vector2';
 
 /**
  * Entity class.
@@ -8,13 +9,19 @@ import { Location } from '../utils/location';
 export abstract class Entity {
 
   public location: Location;
+  public size: Vector2;
+  public color: string;
 
   /**
    * Creates a new entity.
    * 
    * @param location Location
+   * @param size Size
+   * @param color Color
    */
-  protected constructor(location: Location) {
+  protected constructor(location: Location, size: Vector2, color: string) {
     this.location = location;
+    this.size = size;
+    this.color = color;
   }
 }
