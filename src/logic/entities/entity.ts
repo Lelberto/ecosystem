9928d6cf800324b1e247ef6360445125368a7b08
Lelberto <jsurieux@gmail.com>
2021-01-26 16:28:13 +0100

    Add ID on entities

--- src/logic/entities/entity.ts
+++ src/logic/entities/entity.ts
@@ -1,3 +1,4 @@
+import randomString from 'crypto-random-string';
 import { Location } from '../utils/location';
 import { Vector2 } from '../utils/vector2';
 
@@ -8,6 +9,7 @@ import { Vector2 } from '../utils/vector2';
  */
 export abstract class Entity {
 
+  public readonly id;
   public location: Location;
   public size: Vector2;
   public color: string;
@@ -20,6 +22,7 @@ export abstract class Entity {
    * @param color Color in hex string
    */
   protected constructor(location: Location, size: Vector2, color: string) {
+    this.id = `${location.ecosystem.id}_${randomString({ characters: '0123456789abcdef', length: 8 })}`;
     this.location = location;
     this.size = size;
     this.color = color;
